f402005587e152e278a3f9a2a7b8ae3e56317958
olkhon <alexander.stapel@gmail.com>
2021-08-23 13:26:19 +0200

    bug

--- myApp.js
+++ myApp.js
@@ -4,10 +4,7 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", function(req, res) {
-
-    absolutePath = __dirname + /views/index.html;
-
-    res.sendFile(absolutePath);
+    res.sendFile(__dirname + " /views/index.html");
 });
 
 
