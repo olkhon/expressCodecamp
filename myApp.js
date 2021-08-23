b1f75351af224af387914811a46608a6cd25d939
olkhon <alexander.stapel@gmail.com>
2021-08-23 13:36:07 +0200

    bug directory

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + " /views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
