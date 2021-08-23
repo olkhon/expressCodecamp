41b1167bf01499c2af75081d2f4428150f5fac64
olkhon <alexander.stapel@gmail.com>
2021-08-23 13:09:50 +0200

    update bug

--- myApp.js
+++ myApp.js
@@ -3,10 +3,9 @@ var app = express();
 
 console.log("Hello World");
 
-app.get('/', function (req, res) {
-    res.send('Hello Express');
-});
-
+app.get("/", function(req, res) {
+    res.send("Hello Express");
+  });
 
 
 
