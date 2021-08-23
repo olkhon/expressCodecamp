240e435e98040e08971c29741d747f084ea58c6e
olkhon <alexander.stapel@gmail.com>
2021-08-23 14:31:57 +0200

    req, res

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 });
 
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", function (res, req) {
+app.get("/json", function (req, res) {
     res.json({"message": "Hello json"});
     }  );
 
