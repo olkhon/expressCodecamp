2efe2d9fc6314fcf318557b00a9f8a0e1c20023e
olkhon <alexander.stapel@gmail.com>
2021-08-24 09:42:44 +0200

    middleware chained

--- myApp.js
+++ myApp.js
@@ -1,49 +1,26 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 console.log("Hello World");
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
+app.get("/", function (req, res) {
+  res.send("Hello Express");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
-    res.json({"message": "Hello json"});
-    }  );
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date();
+    next();
+  },
+  function (req, res) {
+    res.send({ time: req.time });
+  }
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
